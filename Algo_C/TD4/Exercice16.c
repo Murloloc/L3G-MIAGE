3c9d3d8637f5e53eaa135814dd5ddada2cbbeea6
Murloloc <laurentperez126@gmail.col>
2023-10-28 10:49:18 +0200

    Debut ex 16

--- Algo_C/TD4/Exercice16.c
+++ Algo_C/TD4/Exercice16.c
@@ -2,7 +2,22 @@
 //si le réel appartient ou pas au tableau et s’il appartient, rend le rang de sa première
 //occurrence.
 
-int cherche_vaeleur(float *tab, int n, int x) {
+#include <stdio.h>
+#define N 10
+void rempli_tableau(float *tab, int n) {
+
+    int i;
+
+    i = 0;
+    while (i < n) {
+        printf("Donnez la valeur %d du tableau :\n", i + 1);
+        scanf("%f", &tab[i]);
+        i++;
+    }
+    return;
+}
+
+int cherche_valeur(float *tab, int n, float x) {
 
     int i, indice;
 
@@ -17,6 +32,14 @@ int cherche_vaeleur(float *tab, int n, int x) {
 }
 
 int main() {
+
+    float t[N],valeur_cherchee;
+
+    rempli_tableau(t,N);
+    printf("Donnez la valeur cherchee dans le tableau");
+    scanf("%f",&valeur_cherchee);
+    printf("La premiere occurence de cette valeur %d",cherche_valeur(t,N,valeur_cherchee));
+
 
 
     return 0;
