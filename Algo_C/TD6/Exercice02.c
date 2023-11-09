d31d1ce05d3e593ed77835ad786e74c10e998925
Laurent <laurentperez126@gmail.com>
2023-11-09 23:09:19 +0100

    début ex2 td6

--- Algo_C/TD6/Exercice02.c
+++ Algo_C/TD6/Exercice02.c
@@ -14,3 +14,121 @@
 //Vous définirez la structure de données qui vous semble adaptée, et le programme principal qui
 //demande à l’utilisateur le bénéfice réalisée cette année par l’entreprise et affiche la liste des
 //actionnaires classés
+
+#include <stdio.h>
+
+#define N 2
+
+typedef struct {
+    char nom[80];
+    int nbAction;
+    float dividende;
+} ACTIONNAIRE;
+
+void remplit_tab(ACTIONNAIRE *tab, int n) {
+
+    int i;
+
+    i = 1;
+    while (i <= n) {
+        printf("\nEnregistrement des informations des actionnaires\n");
+        printf("\nVeuillez donner le nom de l'actionnaire %d : ", i);
+        scanf("%s", tab[i].nom);
+        printf("\nDonnez sa quantite d'action :");
+        scanf("%d", &tab[i].nbAction);
+        i++;
+    }
+    return;
+}
+
+int calcule_nb_total_action(ACTIONNAIRE *tab, int n) {
+
+    int s, i;
+
+    i = 1;
+    s = 0;
+
+    while (i <= n) {
+        s = s + tab[i].nbAction;
+        i++;
+    }
+
+    return s;
+}
+
+void calcule_dividende(ACTIONNAIRE *tab, int n, float benef) {
+
+    float dividende;
+    int i;
+
+    i = 1;
+
+    while (i <= n) {
+        dividende = (benef * tab[i].nbAction) / calcule_nb_total_action(tab, n);
+        tab[i].dividende = dividende;
+        i++;
+    }
+    return;
+}
+
+void classe_dividende(ACTIONNAIRE *tab, int n) {
+
+    int i, flag;
+    ACTIONNAIRE ech;
+
+    flag = 1;
+    while (flag == 1) {
+        flag = 0;
+        i = 0;
+        while (i < n - 1) {
+            if (tab[i].dividende < tab[i + 1].dividende) {
+                ech = tab[i];
+                tab[i] = tab[i + 1];
+                tab[i + 1] = ech;
+                flag = 1;
+            }
+            i = i + 1;
+        }
+    }
+    return;
+}
+
+void affichage(ACTIONNAIRE *tab, int n) {
+
+    int i;
+
+    printf("\nVoici le tableau des actionnaires range dans l'ordre decroissant de dividende :\n");
+    i = 1;
+    while (i <= n) {
+        printf("Actionnaire %3d | ", i);
+        printf("Nom actionnaire : %10s\t", tab[i].nom);
+        printf("Nombre d'action : %3d\t", tab[i].nbAction);
+        printf("Dividendes : %5.2f\t", tab[i].dividende);
+        printf("\n");
+        i++;
+    }
+    return;
+}
+
+int main() {
+
+    ACTIONNAIRE t[N];
+    int nombre_total_action;
+    float benefice;
+
+    printf("Donnez le benefice total de la societe : ");
+    scanf("%f", &benefice);
+
+    remplit_tab(t, N);
+
+    nombre_total_action = calcule_nb_total_action(t, N);
+    printf("\nLe nombre total d'action de la societe est : %d\n", nombre_total_action);
+
+    calcule_dividende(t, N, benefice);
+
+//    classe_dividende(t, N);
+
+    affichage(t, N);
+
+    return 0;
+}
