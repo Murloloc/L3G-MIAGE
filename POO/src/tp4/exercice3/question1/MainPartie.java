ede7e85b66e9f388109416c31e71e9b12c607457
Laurent <laurentperez126@gmail.com>
2024-02-29 23:59:17 +0100

    tp4 exo 3 q1

---
+++ POO/src/tp4/exercice3/question1/MainPartie.java
@@ -0,0 +1,10 @@
+package tp4.exercice3.question1;
+
+public class MainPartie {
+    public static void main (String[] args){
+        Belote partie = new Belote();
+
+        partie.jouerPartie(4);
+
+    }
+}
